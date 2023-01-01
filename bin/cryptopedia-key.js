7cc6188405371fee8cef2777459fd24acd28924b
shreyasri258 <kertishreya2508@gmail.com>
2023-01-01 07:16:29 +0530

    starter-template

---
+++ bin/cryptopedia-key.js
@@ -0,0 +1,16 @@
+const program = require('commander');
+const key=require('../commands/key')
+program
+    .command('set')
+    .description('Set API Key -- Get at https://coinmarketcap.com/')
+    .action(key.set);
+program
+    .command('show')
+    .description('Show API Key ')
+    .action(key.show);
+program
+    .command('remove')
+    .description('Remove API Key ')
+    .action(key.remove);
+
+    program.parse(process.argv);
