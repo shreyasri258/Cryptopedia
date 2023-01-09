ffd0a590c735b374d01397d58889fe0b598ae9de
shreyasri258 <kertishreya2508@gmail.com>
2023-01-09 20:11:04 +0530

    added functionalities

--- commands/key.js
+++ commands/key.js
@@ -2,6 +2,7 @@
 const KeyManager=require('../lib/KeyManager')
 const colors=require('colors')
 const inquirer=require('inquirer');
+const {isRequired}=require('../utils/validation')
 const key={
     async set(){
         const keyManager= new KeyManager();
@@ -9,7 +10,8 @@ const input=await inquirer.prompt([
 {
     type:'input',
     name:'key',
-    message:'Enter API key'.green+'https://coinmarketcap.com/'
+    message:'Enter API key'.green+'https://coinmarketcap.com/',
+validate:isRequired
 }
 ]);
 const key=keyManager.SetKey(input.key);
@@ -18,10 +20,22 @@ if(key){
 }
     },
     show(){
-        console.log("hello from show")
+        try {
+            const keyManager=new KeyManager();
+            const key=keyManager.getKey();
+            console.log('current API key ',key.yellow);
+        } catch (error) {
+            console.log(error.message.red);
+        }
     },
     remove(){
-        console.log("hello from remove")
+        try {
+            const keyManager=new KeyManager();
+            keyManager.deleteKey();
+            console.log('key is removed '.blue);
+        } catch (error) {
+            console.log(error.message.red);
+        }
     }
 };
 
