97ffa787b6ca390d085a99115c71f25c409fb327
shreyasri258 <kertishreya2508@gmail.com>
2023-01-12 20:35:41 +0530

    working api

--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,20 @@
+const KeyManager = require('../lib/keyManager');
+const CryptoAPI=require('../lib/CryptoAPI');
+
+
 const check={
-    price(cmd){
-        //console.log(cmd.coin,cmd.cur)
+   async price(cmd){
+       try{
+keyManager=new KeyManager();
+const key =keyManager.getKey();
+
+const api=new CryptoAPI(key);
+const priceOutputData=await api.getpriceData(cmd.coin);
+console.log(priceOutputData);
+       }catch(err){
+        console.error(err.message.red
+            )
+       }
     }
 }
 module.exports=check;
